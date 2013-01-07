5ef0c37728ce5e10d3333d185389d0c0a31d2df6
bstrds <elapoueis@gmail.com>
2013-01-07 14:46:58 +0200

    aw aw

--- Instance.java
+++ Instance.java
@@ -1,35 +1,34 @@
 
 public class Instance {
 	
-	private byte[] vect;
-	private byte value;
+	private double[] vect;
+	private String value;
 	
-	public byte getVal() {
+	public String getVal() {
 		return value;
 	}
 	
-	public byte[] getVec() {
+	public double[] getVec() {
 		return vect;
 	}
 	
 	public Instance() {
-		vect = new byte[10];
-		value = 0;
+		vect = new double[4];
 	}
 	
 	public void fill(String s) {
 		String[] temp;
 		temp = s.split(",");
-		for(int i=0; i<10; i++) {
-			vect[i] = Byte.parseByte(temp[i]);
+		for(int i=0; i<4; i++) {
+			vect[i] = Double.parseDouble(temp[i]);
 		}
-		value = Byte.parseByte(temp[10]);
+		value = temp[4];
 	}
 	
 	public String toString() {
 		String result ="";
 		
-		for(int i=0; i<10; i++) {
+		for(int i=0; i<vect.length; i++) {
 			result += vect[i]+",";
 		}
 		result += value;
--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
 		
 		try 
 		{
-			FileInputStream fstream = new FileInputStream("poker-hand-training-true.data");
+			FileInputStream fstream = new FileInputStream("irisnew");
 			DataInputStream in = new DataInputStream(fstream);
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			
@@ -23,48 +23,8 @@ public class Main {
 		}
 		
 		//int le = ip.getNum();
-		//System.out.println(ip);
+		System.out.println(ip);
 		
-		Bayes b = new Bayes();
-		b.train(ip);
-		System.out.println(b);
-		double[][] cops = b.getCops();
-		for(int i=0; i<13; i++) {
-			System.out.println("token "+(i+1));
-			for(int j=0; j<10; j++) {
-				System.out.println("probability of category "+j+" = "+cops[i][j]);
-			}
-		}
 		
-		InstancePool testPool = new InstancePool();
-		
-		try 
-		{
-			FileInputStream fstream = new FileInputStream("poker-hand-testing.data");
-			DataInputStream in = new DataInputStream(fstream);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			
-			String line;
-			
-			while((line = br.readLine()) != null) {
-				testPool.fill(line);
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("error");
-		}
-		
-		int c = 0;
-		
-		System.out.println(testPool.getNum());
-		
-		for(Instance inst : testPool.getList()) {
-			if(b.classify(inst)==inst.getVal()) {
-				c++;
-			}
-		}
-		
-		System.out.println("Success ratio : "+(double)c/testPool.getNum());
-
 	}
 }
